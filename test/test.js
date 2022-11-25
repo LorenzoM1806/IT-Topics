321505856ecae2ae477a42ca14f55b2dbbe56433
lorenzoM1806 <lorenzomiechielsen@hotmail.com>
2022-11-25 15:01:14 +0100

    testaangepast

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const request = require ('supertest');
+const fs = require("os");
 //test 2
 describe('get campus', () => {
     it('should return first campus', async () => {
@@ -7,7 +8,17 @@ describe('get campus', () => {
         .expect(200)
         .expect('content-type', 'application/json; charset=utf-8')
         .expect((res) => {
+            fs.writeFile("output.json", JSON.stringify(res.body[0]), 'utf8', function (err) {
+                if(err) {
+                    console.log("An error occured while JSON object to file.");
+                    return console.log(err);
+                }
+
+                console.log("JSON file has been saved.");
+            });
+
             console.log(res.body[0])
+            
         })
     });
 });
